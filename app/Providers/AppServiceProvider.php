2a3a5bb95f01be428cce3bfae8d55e20cf4379b1
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2019-06-21 16:40:55 +0300

    Fixed mysql key length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Http\Services\HashService;
 use App\Http\Services\UrlService;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         $this->app->singleton(HashService::class);
         $this->app->singleton(UrlService::class);
 
